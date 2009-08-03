20728cf28365e8578ddd67b5e36b5a0daf6ab8a2
David Schmitt <david@dasz.at>
2009-08-03 11:39:55 +0200

    Add munin_interfaces support for FreeBSD and Solaris
    This enhancement made by Nicolas SZALAY
    <http://www.rottenbytes.info/?p=192>, brought to you via the new
    planetpuppet.com.

--- plugins/facter/munin_interfaces.rb
+++ plugins/facter/munin_interfaces.rb
@@ -1,16 +1,36 @@
 # return the set of active interfaces as an array
 Facter.add("munin_interfaces") do
-	setcode do
+	# linux
+	if Facter.value('kernel') == "Linux" then
 		`ip -o link show`.split(/\n/).collect do |line|
 			value = nil
 			matches = line.match(/^\d*: ([^:]*): <(.*,)?UP(,.*)?>/)
 			if !matches.nil?
 				value = matches[1]
-				# remove superfluous "underlying interface" specification 
-				# for VLAN interfaces
 				value.gsub!(/@.*/, '')
 			end
 			value
-		end.compact.sort.join(" ")
-	end
+		end
+	# freebsd
+	elsif Facter.value('kernel') == "FreeBSD" then
+		Facter.value('interfaces').split(/,/).collect do |interface|
+			status = `ifconfig #{interface} | grep status`
+			if status != "" then
+				status=status.strip!.split(":")[1].strip!
+				if status == "active" then # I CAN HAZ LINK ?
+					interface.to_a
+				end
+			end
+		end
+	# solaris
+	elsif Facter.value('kernel') == "SunOS" then
+		Facter.value('interfaces').split(/,/).collect do |interface|
+			if interface != "lo0" then # /dev/lo0 does not exists
+				status = `ndd -get /dev/#{interface} link_status`.strip!
+				if status == "1" # ndd returns 1 for link up, 0 for down
+					interface.to_a
+				end
+			end
+		end
+	end.compact.sort.join(" ")
 end
